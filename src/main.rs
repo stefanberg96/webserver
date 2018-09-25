64fc81643efef4dce9be58df0ee4c5ac3d6f33b1
Stefan van den Berg <bergshm@gmail.com>
2018-09-25 17:05:54 +0200

    changed of specifying port to specifying ip

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,10 @@ extern crate clap;
 
 
 use std::io::prelude::*;
-use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream};
 use std::thread;
 use clap::{App,Arg};
 
-
-fn is_valid_port(v: String) -> Result<(), String>{
-    let value = v.parse::<u16>();
-    match value{
-        Ok(_val) => return Ok(()),
-        _ => Err(String::from("The port was not a valid number"))
-    }
-}
 
 fn main() {
 
@@ -21,19 +13,14 @@ fn main() {
     let matches = App::new("My default_web_app")
         .version("0.1")
         .author("Stefan")
-        .about("Listens on port p and responds with hello world.")
-        .arg(Arg::with_name("port")
-            .short("p")
-            .long("port")
+        .about("Listens on ip  and responds with hello world.")
+        .arg(Arg::with_name("ip")
+            .long("ip")
             .required(true)
-            .validator(is_valid_port)
             .help("Sets the port to listen on")
             .takes_value(true)).get_matches();
 
-    let port =  matches.value_of("port").unwrap().parse::<u16>().unwrap();
-    let addr = SocketAddr::from(([192, 168, 1, 230], port));
-    let listener = TcpListener::bind(addr).unwrap();
-    println!("started listening on {}",addr);
+    let listener = TcpListener::bind(matches.value_of("ip").unwrap()).unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
